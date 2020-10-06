6e4f4f13c5fdcc24cab01f99db9f545d730dea0c
Kimberly Flores <kimberlyflrs@users.noreply.github.com>
2020-10-05 23:15:20 -0700

    fixed private routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,31 +1,50 @@
 import React from 'react';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.css';
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { BrowserRouter as Router, Route, Switch, Redirect } from "react-router-dom";
 import LandingPage from './pages/LandingPage';
 import ViewAllRecipe from './pages/ViewAllRecipe';
 import AddRecipe from './pages/AddRecipe';
 import EditRecipe from './pages/EditRecipe';
 import SingleRecipe from './pages/SingleRecipe';
-import PrivateRoute from './components/PrivateRoute';
-
 
 /*
-if no token, show the login page
+if no token or expired, show the login page
 if token valid, show the view all recipe page
 
 when going to pages make sure they are logged in and token is not expired
 */
+function PrivateRoute({ children, ...rest }) {
+  return (
+    <Route
+      {...rest}
+      render={({ location }) =>
+        localStorage.getItem('token') ? (
+          children
+        ) : (
+          <Redirect
+            to={{
+              pathname: "/",
+              state: { from: location }
+            }}
+          />
+        )
+      }
+    />
+  );
+}
+
+
 
 function App() {
   return (
     <Router>
       <Switch>
         <Route exact path="/" component={LandingPage}></Route>
-        <Route exact path="/addRecipe" component={AddRecipe}></Route>
-        <Route exact path="/viewRecipes" component={ViewAllRecipe}></Route>
-        <Route exact path="/recipe/edit/:editRecipe" render={(props) => <EditRecipe {...props}/>}></Route>
-        <Route exact path="/recipe/view/:id" render={(props) => <SingleRecipe {...props}/>}></Route>
+        <PrivateRoute exact path="/addRecipe" component={AddRecipe}></PrivateRoute>
+        <PrivateRoute exact path="/viewRecipes" component={ViewAllRecipe}></PrivateRoute>
+        <PrivateRoute exact path="/recipe/edit/:editRecipe" component={EditRecipe} render={(props) => <EditRecipe {...props}/>}></PrivateRoute>
+        <PrivateRoute exact path="/recipe/view/:id" component={SingleRecipe} render={(props) => <SingleRecipe {...props}/>}></PrivateRoute>
       </Switch>
     </Router>
   );
