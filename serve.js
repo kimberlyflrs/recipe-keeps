3b53b9468115a884d35556335519281284b2aa01
Kimberly Flores <kimberlyflrs@users.noreply.github.com>
2020-10-29 22:39:55 -0700

    Update serve.js

--- serve.js
+++ serve.js
@@ -33,10 +33,10 @@ app.use("/api/foodentries", require("./recipes"));
 if (process.env.NODE_ENV === "production") {
     // set static folder
     // Serve static files from the React frontend app
-    app.use(express.static(path.join(__dirname, 'client/build')))
+    app.use(express.static(path.join(__dirname, '../client/build')))
     // Anything that doesn't match the above, send back index.html
     app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname + '/client/build/index.html'))
+    res.sendFile(path.join(__dirname + '../client/build/index.html'))
     })
   }
 
